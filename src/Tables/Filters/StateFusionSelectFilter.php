53dcceba99252600d8fd8d61b64aacf89533456e
Assem Alwaseai <119125167+A909M@users.noreply.github.com>
2025-09-08 19:44:47 +0300

    refactor(filters): simplify StateFusionSelectFilter implementation
    - Removed HasStateAttributes trait and contract dependency
    - Simplified class structure while maintaining functionality
    - Fixed minor formatting in options callback

--- src/Tables/Filters/StateFusionSelectFilter.php
+++ src/Tables/Filters/StateFusionSelectFilter.php
@@ -2,18 +2,14 @@
 
 namespace A909M\FilamentStateFusion\Tables\Filters;
 
-use A909M\FilamentStateFusion\Concerns\HasStateAttributes;
-use A909M\FilamentStateFusion\Contracts\HasStateAttributesContract;
 use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
 
-class StateFusionSelectFilter extends SelectFilter implements HasStateAttributesContract
+class StateFusionSelectFilter extends SelectFilter
 {
-    use HasStateAttributes;
-
     protected function setUp(): void
     {
         parent::setUp();
-        $this->options(fn (Table $table) => (new ($table->getModel()))->getCasts()[$this->getAttribute()]::getStatesLabel($table->getModel()));
+        $this->options(fn(Table $table) => (new ($table->getModel())())->getCasts()[$this->getAttribute()]::getStatesLabel($table->getModel()));
     }
 }
